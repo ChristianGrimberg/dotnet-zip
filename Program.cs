2e84fddb9fba7ec29f408e31026e373032e36a82
Christian Grimberg <christian.grimberg@outlook.com>
2020-02-11 21:12:10 -0300

    :rocket: Aplicacion de consola de .net core para comprimir archivos a partir del ingreso de dos argumentos.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace dotnet_zip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = null;
+            string target = null;
+
+            if(args == null || args.Length != 2)
+            {
+                Console.WriteLine("Uso: <origen> <destino>.zip");
+                return;
+            }
+
+            source = args[0];
+            target = args[1];
+
+            ZipFile.CreateFromDirectory(source, target);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# My first .NET Core Console Application
+This app is development with .NET Core 3.1 Framework.
